e191c8018c2c42c22f0adb070b407e0c2159a4bc
nıdaturkay <145764089+NdaTrky@users.noreply.github.com>
2024-11-29 18:54:11 +0300

    proje yapıldı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import org.example.entity.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Set<Task> annsTasks = new HashSet<>();
+        Task taskAnn = new Task("workintech", "dummy desc", "ann", Status.IN_PROGRESS, Priority.HIGH);
+        Task taskAnn1 = new Task("workintech", "dummy desc1", "ann", Status.IN_PROGRESS, Priority.HIGH);
+        annsTasks.add(taskAnn);
+        annsTasks.add(taskAnn1);
+
+        Set<Task> bobsTasks = new HashSet<>();
+        Task taskBob = new Task("workintech", "dummy", "bob", Status.IN_PROGRESS, Priority.HIGH);
+        Task taskBob1 = new Task("workintech", "dummy1", "bob", Status.IN_PROGRESS, Priority.HIGH);
+        bobsTasks.add(taskBob);
+        bobsTasks.add(taskBob1);
+
+        Set<Task> carolTasks = new HashSet<>();
+        Task taskCarol = new Task("workintech", "dummy", "carol", Status.IN_PROGRESS, Priority.HIGH);
+        Task taskCarol1 = new Task("workintech", "dummy1", "carol", Status.IN_PROGRESS, Priority.HIGH);
+        carolTasks.add(taskCarol);
+        carolTasks.add(taskCarol1);
+
+
+        Set<Task> unassignedTasks = new HashSet<>();
+        Task unassignedTask = new Task("workintech", "dummy", null, Status.IN_PROGRESS, Priority.HIGH);
+        unassignedTasks.add(unassignedTask);
+
+        TaskData taskData = new TaskData(annsTasks, bobsTasks, carolTasks, unassignedTasks);
+        System.out.println("bobtasks: "+taskData.getTasks("bob"));
+        System.out.println("annytaks:"+taskData.getTasks("ann"));
+        System.out.println("alltasks: "+taskData.getTasks("all"));
+        taskData.getIntersection(annsTasks,bobsTasks);
+        taskData.getIntersection(annsTasks, carolTasks);
+        taskData.getIntersection(bobsTasks, carolTasks);
+
+        System.out.println("unassigned:" + taskData.getDifferences(unassignedTasks,taskData.getTasks("ann")));
+
+        System.out.println(("*******************************************************"));
+
+        StringSet.findUniqueWords();
     }
+
 }
---
+++ src/main/java/org/example/entity/TaskData.java
@@ -0,0 +1,68 @@
+package org.example.entity;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class TaskData {
+    private Set<Task> annsTasks;
+    private Set<Task> bobsTasks;
+    private Set<Task> carolsTasks;
+    private Set<Task> unassignedTasks;
+
+
+    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks, Set<Task> carolsTask, Set<Task> unassignedTasks) {
+        this.annsTasks = annsTasks;
+        this.bobsTasks = bobsTasks;
+        this.carolsTasks = carolsTasks;
+        this.unassignedTasks = unassignedTasks;
+    }
+    public Set<Task> getTasks(String name){ //Bu metod, ilgili isme göre ilgili kişinin taskını verir.
+        switch (name){
+            case "ann":
+                return this.annsTasks;
+            case "bob":
+                return this.bobsTasks;
+            case "carol":
+                return this.carolsTasks;
+            case "all":
+                return getUnion(bobsTasks,annsTasks,carolsTasks);
+            default:
+        }
+        return new HashSet<>();
+    }
+
+    public Set<Task> getUnion(Set<Task>...sets) { // js'deki spread operatoru gibi, bu kullanımda istersek 1 istersek 3 parametre gönderebiliriz. Tek şart veri tipinin task set olması gerekmektedir.
+        HashSet<Task> allTask = new LinkedHashSet<>();
+        for(Set<Task> tasks : sets){
+            allTask.addAll(tasks);
+        }
+        return allTask;
+    }
+    public Set<Task> getIntersection(Set<Task> first, Set<Task> second){
+        Set<Task> intersection = new HashSet<>(first);
+        intersection.retainAll(second);
+        return intersection;
+    }
+    public Set<Task> getDifferences(Set<Task> first, Set<Task> second){
+        Set<Task> difference = new HashSet<>(first);
+        difference.removeAll(second); //first içindeki elemanlar ile second içindeki elemanları kıyaslar. Firstte olan, secondda olmayanları difference'de tutar ve döner.
+        return difference ;
+    }
+
+    public Set<Task> getAnnsTasks() {
+        return annsTasks;
+    }
+
+    public Set<Task> getBobsTasks() {
+        return bobsTasks;
+    }
+
+    public Set<Task> getCarolsTask() {
+        return carolsTasks;
+    }
+
+    public Set<Task> getUnassignedTasks() {
+        return unassignedTasks;
+    }
+}
